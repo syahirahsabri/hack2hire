e06224ec15bfc61bf14dc1ec9066868f6e5ecf76
syahirahsabri <syahirahnoorsabri@gmail.com>
2023-10-12 13:13:54 +0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Dashboard.jsx";
+import Form from "./components/form";
+
 
 // function App() {
 //   const [count, setCount] = useState(0)
@@ -38,6 +40,8 @@ export default function App() {
     <>
       {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
       <Dashboard />
+      <Form/>
+      
     </>
   );
 }
