5f1c313a1e23af8237a6fbbb5dc3a2e26e71aeed
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-10-14 18:47:32 +0800

    Implement form calculation

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,7 +1,6 @@
-import { QuantityInput } from "../components/QuantityInput";
-import { DropDown } from "../components/Dropdown";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Listbox } from "@headlessui/react";
 
 const electricalAppliances = [
   { id: 1, name: "Air-Conditioner" },
@@ -18,11 +17,164 @@ const dropdownConfigs = [
   { title: "CCTV", levels: ["5", "5", "5"] },
   { title: "Computer", levels: ["5", "20", "3"] },
   { title: "Router/Modem", levels: ["4", "4", "2"] },
-  { title: "Lift", levels: ["3", "3", "3"] },
+  { title: "Lift", levels: ["3", "0", "0"] },
 ];
 
-function CalculateElectricalUsage() {
-  //Calculation!
+const electricalConsumption = [
+  { title: "Airconds", power: "0.8" },
+  { title: "Pendaflour", power: "0.2" },
+  { title: "CCTV", power: "0.015" },
+  { title: "Computer", power: "0.021" },
+  { title: "Router/Modem", power: "0.0005" },
+  { title: "Lift", power: "0.5" },
+];
+
+const defaultValue = 3;
+
+function calculateTotalPowerConsumption(
+  dropdownConfigs,
+  electricalConsumption,
+  defaultValue
+) {
+  if (
+    !Array.isArray(dropdownConfigs) ||
+    !Array.isArray(electricalConsumption)
+  ) {
+    console.error("Invalid data format. Arrays are expected.");
+    console.log(dropdownConfigs);
+    return "Tak dapat value"; // Return 0 or handle the error as needed
+  }
+
+  const totalUnitsByComponent = {};
+  let totalPowerConsumption = 0;
+
+  dropdownConfigs.forEach((config) => {
+    const componentTitle = config.title;
+    totalUnitsByComponent[componentTitle] = 0;
+
+    config.levels.forEach((level) => {
+      totalUnitsByComponent[componentTitle] += parseInt(level, 10);
+    });
+  });
+
+  electricalConsumption.forEach((item) => {
+    const componentTitle = item.title;
+    if (totalUnitsByComponent[componentTitle]) {
+      const power = parseFloat(item.power);
+      const units = totalUnitsByComponent[componentTitle];
+      totalPowerConsumption += power * units * defaultValue;
+    }
+  });
+
+  return totalPowerConsumption;
+}
+
+function QuantityInput({ level1, level2, level3, showLevels }) {
+  const [hoursUsed, setHoursUsed] = useState(0);
+
+  const handleInputChange = (e) => {
+    const newValue = e.target.value;
+    setHoursUsed(newValue);
+    onInputChange(newValue);
+  };
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="mt-4 flex flex-col justify-center items-center p-4 rounded-md border border-gray-300 shadow-xl bg-white">
+        <label className="text-sm font-medium text-gray-900">
+          <b>Quantity</b>
+        </label>
+        <div className="mt-2 flex flex-col items-center justify-center">
+          <label
+            htmlFor="level_1"
+            className="block text-sm font-medium text-gray-700"
+          >
+            {showLevels ? "Level 1" : ""}
+          </label>
+          <input
+            name="level_1"
+            id="level_1"
+            className="w-[20%] h-8 border border-gray-300 rounded-md px-2 py-1 text-gray-700 focus:outline-none"
+            type="text"
+            defaultValue={level1}
+            disabled
+          />
+        </div>
+        {showLevels && (
+          <>
+            <div className="mt-2 flex flex-col items-center justify-center">
+              <label
+                htmlFor="level_2"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Level 2
+              </label>
+              <input
+                name="level_2"
+                id="level_2"
+                className="w-[20%] h-8 border border-gray-300 rounded-md px-2 py-1 text-gray-700 focus:outline-none"
+                type="text"
+                defaultValue={level2}
+                disabled
+              />
+            </div>
+            <div className="mt-2 flex flex-col items-center justify-center">
+              <label
+                htmlFor="level_3"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Level 3
+              </label>
+              <input
+                name="level_3"
+                id="level_3"
+                className="w-[20%] h-8 border border-gray-300 rounded-md px-2 py-1 text-gray-700 focus:outline-none"
+                type="text"
+                defaultValue={level3}
+                disabled
+              />
+            </div>
+          </>
+        )}
+      </div>
+      {/* Hours used */}
+      <div className="mt-5 flex flex-col justify-center items-center">
+        <label className="text-sm font-medium text-gray-900">
+          <b>Hours used</b>
+        </label>
+        <input
+          type="text"
+          name="level_3"
+          id="level_3"
+          onChange={handleInputChange}
+          value={hoursUsed}
+          className="flex flex-col items-center justify-center w-[20%] h-8 border border-gray-300 rounded-md px-2 py-1 text-gray-700 focus:outline-none"
+        />
+      </div>
+    </div>
+  );
+}
+
+function DropDown({ title, options, selectedOption, onDropdownChange }) {
+  const handleChange = (selectedValue) => {
+    const selectedOptionIndex = options.findIndex(
+      (option) => option.id === selectedValue.id
+    );
+    onDropdownChange(selectedOptionIndex);
+  };
+
+  const selected = options[selectedOption];
+
+  return (
+    <div className="w-72 mr-4 flex flex-col items-center justify-center">
+      <Listbox value={selected} onChange={handleChange}>
+        <>
+          <label className="mt-8 text-sm font-semibold text-gray-900">
+            <b>{title}</b>
+          </label>
+        </>
+      </Listbox>
+    </div>
+  );
 }
 
 export default function Form() {
@@ -42,11 +194,12 @@ export default function Form() {
     }));
   };
 
+  const handleCalculator = () => {
+    
+  }
+
   return (
     <>
-      {/* <div className="flex flex-col items-center justify-center mt-5 mb-0">
-        <h1 className="text-xl font-bold text-4xl">Modify building info</h1>
-      </div> */}
       <div className="mt-10 ml-[13%] bg-white w-[75%] p-4 rounded-md border border-gray-300 shadow-xl">
         <form className="flex flex-col justify-center items-center">
           <div className="flex flex-wrap justify-between p-8 py-3">
@@ -65,10 +218,18 @@ export default function Form() {
                   level2={config.levels[1]}
                   level3={config.levels[2]}
                   showLevels={selectedOptions[`dropdown${index + 1}`] !== 5}
+                  onInputChange={(hours) =>
+                    handleHoursChange(`dropdown${index + 1}`, hours)
+                  }
                 />
               </div>
             ))}
           </div>
+          {calculateTotalPowerConsumption(
+            dropdownConfigs,
+            electricalConsumption,
+            defaultValue
+          )}
         </form>
       </div>
 
@@ -83,7 +244,8 @@ export default function Form() {
           </button>
         </Link>
         <button
-          type="submit"
+          type="button"
+          onClick={() => handleCalculator()}
           className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Save
