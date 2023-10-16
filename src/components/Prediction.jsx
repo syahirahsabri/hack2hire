b858842b43f633fb0886c506b6bc2bdbaac66d8a
Ahmad Rusyaidi <ahmad.rusyaidiGmail.com>
2023-10-16 12:05:08 +0800

    fix dashboard

--- src/components/Prediction.jsx
+++ src/components/Prediction.jsx
@@ -6,12 +6,20 @@ const PredictionDatas = {
       value: "55 kWh",
     },
     {
+      type: "Current Monthly Bill",
+      value: "RM 10,351",
+    },
+    {
       type: "Expected Monthly Bill",
-      value: "RM 921",
+      value: "RM 12,000",
+    },
+    {
+      type: "Current Carbon Emission",
+      value: "956 tons",
     },
     {
       type: "Expected Carbon Emission",
-      value: "520 tons",
+      value: "1000 tons",
     },
   ],
 };
@@ -19,7 +27,7 @@ const PredictionDatas = {
 function PredictionComponent({ data }) {
   return (
     <div
-      className="flex flex-col justify-center items-center rounded-lg border border-solid border-black bg-white p-6"
+      className="flex flex-col justify-center items-center rounded-lg border border-solid border-black bg-white p-5"
       style={{ boxShadow: "6px 6px rgb(0, 0, 0)" }}
     >
       <p className="text-[#636262]">{data.type}</p>
@@ -35,8 +43,8 @@ function PredictionComponent({ data }) {
 export default function Prediction() {
   return (
     <section className="relative">
-      <div className="mx-auto max-w-7xl px-5 py-4 md:px-10 md:py-6 lg:py-8">
-        <div className="mb-2 flex flex-row space-x-5 md:mb-12 lg:mb-16">
+      <div className="p-2 mx-auto max-w-7xl px-5 py-4 md:px-2 lg:py: md:py-6 lg:py-8">
+        <div className="mb-0 flex flex-row space-x-3 md:mb-12 lg:mb-0">
           {PredictionDatas.datas.map((data, index) => (
             <PredictionComponent data={data} key={index} />
           ))}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,8 +6,9 @@ function PowerBIDashboard() {
     <>
       <iframe
         title="Full Dashboard"
-        width="1140"
-        height="541.25"
+        width="1300"
+        height="700"
+        className="border-2 border-black"
         src="https://app.powerbi.com/reportEmbed?reportId=00ffccd9-4c97-494b-913f-53996629335b&autoAuth=true&ctid=1f551aeb-7ea1-472c-9ac0-09de9bf33051"
         // frameborder="100"
         allowFullScreen="true"
@@ -19,7 +20,7 @@ function PowerBIDashboard() {
 export default function Dashboard() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center px-5 py-16 md:px-10 md:py-24 lg:py-32">
+      <div className="flex flex-col items-center justify-center px-5 py-16 md:px-10 md:py-24 lg:py-20">
         <div className="mb-8 w-full max-w-3xl text-center md:mb-12 lg:mb-16">
           <h2 className="text-3xl font-bold mb-5 md:text-5xl">👩‍💻</h2>
           <h2 className="text-3xl mb-5 font-bold md:text-5xl">
@@ -28,20 +29,7 @@ export default function Dashboard() {
           <div>
             <Prediction />
           </div>
-          <div className="flex flex-col justify-center w-800 items-center z-0 font-bold md:text-5xl">
-            <PowerBIDashboard />
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-2 lg:w-[50%]">
-          {/* Form div */}
-          <div className="flex max-w-md flex-col items-center justify-center rounded-md border border-[#cdcdcd] p-8">
-            <p className="mb-6 text-base font-light text-[#636262] md:mb-10 lg:mb-12">
-              Your current annual electricity usage is
-            </p>
-            <h2 className="mb-5 text-3xl font-semibold md:mb-6 md:text-5xl lg:mb-12">
-              RM 10351
-            </h2>
+          <div className="p-2 mb-6">
             <Link to={"/form"}>
               <a
                 className="inline-block rounded-xl bg-black px-8 py-4 text-center font-semibold text-white"
@@ -51,31 +39,10 @@ export default function Dashboard() {
               </a>
             </Link>
           </div>
-
-          {/* Calculator div */}
-          <div className="flex max-w-md flex-col items-center justify-center rounded-md border border-[#cdcdcd] p-8">
-            <p className="mb-6 text-base font-light text-[#636262] md:mb-10 lg:mb-12">
-              Your current annual carbon emission is
-            </p>
-            <h2 className="mb-5 text-3xl font-semibold md:mb-6 md:text-5xl lg:mb-12">
-              956 tons
-            </h2>
-            <Link to={"/calculator"}>
-              <a
-                className="inline-block rounded-xl bg-black px-8 py-4 text-center font-semibold text-white"
-                style={{ boxShadow: "6px 6px rgb(192,192,192)" }}
-              >
-                Reduce the emissions
-              </a>
-            </Link>
+          <div className="flex flex-col justify-center w-800 items-center z-0 font-bold md:text-5xl">
+            <PowerBIDashboard />
           </div>
         </div>
-        <a
-          href="/"
-          className="mb-5 w-[20%] mt-10 py-3 text-center font-semibold text-black md:mb-6 lg:mb-8 hover:bg-white hover:text-black"
-        >
-          👈🏻 Return to landing
-        </a>
       </div>
     </>
   );
